7796e192aa7734ef35580db96c142a668fd211a9
akorton <akortysh@yandex.ru>
2022-08-29 19:52:46 +0300

    Add validation for y input element

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const offsetY = 30;
 const steps = {'x': (width - offsetX) / (2*maxCoord), 'y': (height - offsetY) / (2*maxCoord)};
 const origin = {'x': offsetX / 2 + maxCoord * steps['x'], 'y': offsetY / 2 + maxCoord * steps['y']};
 const radiusButtons = document.getElementsByName('radius');
+const yButton = document.getElementById('y');
 
 let setUp = ()=>{
     ctx.moveTo(origin.x - steps.x * 4, origin.y);
@@ -50,4 +51,14 @@ let drawQuaterCircle = (r)=>{
 setUp();
 radiusButtons.forEach((btn)=>{
     btn.addEventListener('click', (e)=>(draw(btn.value)));
+});
+
+yButton.addEventListener('input', (e)=>{
+    let integerValue = +yButton.value;
+    if (Number.isNaN(integerValue) && yButton.value != '-'){
+        yButton.value = yButton.value.substring(0, yButton.value.length - 1);
+    }
+    else if (integerValue > 3 || integerValue < -3){
+        yButton.value = yButton.value.substring(0, yButton.value.length - 1);
+    }
 });
